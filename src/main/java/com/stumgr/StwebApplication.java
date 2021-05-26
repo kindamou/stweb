7eb9b5f29d0ed09f7fb1582647ab77b944b3206f
kindamou <710800480@qq.com>
2021-05-26 15:13:35 +0800

    first commit

---
+++ src/main/java/com/stumgr/StwebApplication.java
@@ -0,0 +1,13 @@
+package com.stumgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stumgr/StwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stumgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
